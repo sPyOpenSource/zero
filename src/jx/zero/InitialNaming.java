884136d7fe76d4e42d51bae7c0e05704aefe1d9a
X. Wang <xuyi@tetra.nl>
2025-01-25 15:37:54 +0100

    save

--- src/jx/zero/InitialNaming.java
+++ src/jx/zero/InitialNaming.java
@@ -1,5 +1,8 @@
 package jx.zero;
 
 public class InitialNaming {
-    public static native Naming getInitialNaming();
+    //public static Naming naming;
+    public static Naming getInitialNaming(){
+        return null;
+    }
 }
